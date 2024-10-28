6b3a0f759704b9b1d8d4f4ea523c1625127c2d20
Lê Khai Trí <lekhaitri.2003@gmail.com>
2024-10-28 13:23:35 +0700

    add db context

---
+++ MVC-View/Models/Account.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVC_View.Models;
+
+public partial class Account
+{
+    public string UserName { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+}
---
+++ MVC-View/Models/Catalog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVC_View.Models;
+
+public partial class Catalog
+{
+    public int Id { get; set; }
+
+    public string? CatalogCode { get; set; }
+
+    public string? CatalogName { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
