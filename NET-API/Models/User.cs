a461516e3a2225565334c5a2a88b81374e133478
Lê Khai Trí <lekhaitri.2003@gmail.com>
2024-10-28 15:41:40 +0700

    add user controller for api layer

--- NET-API/Models/User.cs
+++ NET-API/Models/User.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 
 namespace NET_API.Models;
 
-public partial class Account
+public partial class User
 {
-    public string UserName { get; set; } = null!;
+    public string Username { get; set; } = null!;
 
     public string Password { get; set; } = null!;
+
+    public string? Fullname { get; set; }
 }
